fcf80e7ad81d1fe6ad33d8c8ce10d936c9630809
meisteregor <33342704+meisteregor@users.noreply.github.com>
2019-06-14 13:33:46 +0300

    Update log_parser.py

--- home05_log_parser/log_parser.py
+++ home05_log_parser/log_parser.py
@@ -1,1 +1,10 @@
+def entry_point():
+    print("Hello, interactive mode of log_parser.py is running.")
+    while True:
+        intercept = input("Enter an option: ")
+        # nesting
+
+
+if __name__ == '__main__':
+    entry_point()
 
