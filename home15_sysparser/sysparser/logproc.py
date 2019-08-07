e94ff3536dce0f95cd2f6641c419e82d5cc1e747
meisteregor <33342704+meisteregor@users.noreply.github.com>
2019-08-07 22:44:06 +0300

    Add files via upload

--- home15_sysparser/sysparser/logproc.py
+++ home15_sysparser/sysparser/logproc.py
@@ -3,15 +3,13 @@
 import argparse
 import sys
 
-SEVERITIES = ['CRITICAL', 'WARN', 'ERROR', 'INFO', 'DEBUG']
-
 parser = argparse.ArgumentParser()
 parser.add_argument('input', help="input logfile")
-parser.add_argument('--level', help="severities, all include by default", type=str, nargs='+', default=SEVERITIES,
-                    choices=SEVERITIES)
-parser.add_argument('--app', help="apps to grep from log", type=str, nargs='+')
+parser.add_argument('--level', help="severities, all include by default", nargs='+')
+parser.add_argument('--app', help="apps to grep from log", nargs='+')
 parser.add_argument('--output', type=argparse.FileType('w'), help="set file for output, screen by default")
 args = parser.parse_args()
+
 
 severities, apps = args.level, args.app
 
@@ -22,11 +20,7 @@ class NotValidDataError(Exception):
 
 def is_valid_data(arr):
     for substring in arr:
-        checker_array = []
-        for level in SEVERITIES:
-            if level in substring:
-                checker_array.append(level)
-        if len(checker_array) != 1:
+        if len(substring.split(": ")) != 4:
             return False
     return True
 
@@ -34,7 +28,7 @@ def is_valid_data(arr):
 def take_content(filename):
     try:
         with open(filename) as f:
-            content = f.readlines()
+            content = list(map(lambda s: s.strip(), f.readlines()))
             if is_valid_data(content):
                 return content
             else:
@@ -52,6 +46,8 @@ def write_content(filename, content):
 
 def filtering(content_list, filter_array):
     filtered_list = []
+    if filter_array is None:
+        return content_list
     for substring in content_list:
         for element in filter_array:
             if element in substring:
